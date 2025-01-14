55ee89caba5a46a985a4a40d394e64e53d1e7070
Poof Young <poof@coding4kicks.com>
2025-01-13 20:44:25 -0800

    Update favicon

--- site/up-only/app/layout.tsx
+++ site/up-only/app/layout.tsx
@@ -9,8 +9,28 @@ import Navbar from '@/components/navbar';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'UpOnly',
-  description: 'NFT Collection'
+  title: 'UpOnly NFTs',
+  description: 'UpOnly NFT Collection',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
+    ],
+    apple: [{ url: '/apple-touch-icon.png' }],
+    other: [
+      {
+        url: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        url: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
+  }
 };
 
 export default function RootLayout({
